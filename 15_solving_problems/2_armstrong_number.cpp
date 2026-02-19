6d12b13173aa0f005716e702b172b63a70825f02
kamlesh kumar <coderkamlesg68@gmail.com>
2026-02-20 00:44:16 +0530

    Add armstrong number solution with notes + .gitignore for exe files

--- 15_solving_problems/2_armstrong_number.cpp
+++ 15_solving_problems/2_armstrong_number.cpp
@@ -1,24 +1,45 @@
 #include <iostream>
 using namespace std;
 
+int countDigits(int n){
+    int count=0;
+    while(n>0){
+        n=n/10;
+        count++;
+    }
+    return count;
+}
+
 bool isArmstrong(int n){
    int originalnumber=n;
-   int sum=0;
-   while(n>0){
-    int digit=n%10;
-    sum+=digit*digit*digit;
-    n=n/10;
-   }
-   if(sum==originalnumber){
-    return true;
-   }
-   else{
-    return false;
-   }
-   
+    int sum=0;
+    while(n>0){
+        int digit=n%10;
+        sum+=digit*digit*digit;
+        n=n/10;
+    }
+   return sum==originalnumber;
+}
+
+bool isArmstrong(int n, int digits){
+   int originalnumber=n;
+    int sum=0;
+    while(digits--){
+        int digit=n%10;
+        sum+=digit*digit*digit;
+        n=n/10;
+    }
+   return sum==originalnumber;
 }
 
 int main() {
    
+    int input;
+    cin>>input;
+    int diguts=countDigits(input);
+    // cout<<countDigits(input)<<endl;
+    // cout<<isArmstrong(input)<<endl;
+    cout<<isArmstrong(input,diguts)<<endl;
+
     return 0;
 }
